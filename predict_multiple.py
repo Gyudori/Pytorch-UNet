1b9e08888b70fcff176035774d6e859e2fa50882
ben.lee <ben.lee@cupix.com>
2025-04-14 11:04:11 +0900

    update predict_multiple.py

--- predict_multiple.py
+++ predict_multiple.py
@@ -1,25 +1,16 @@
 from pathlib import Path
-from PIL import Image
 import torch
+from torchvision.utils import save_image
+import numpy as np
 
 from unet import UNet
-from predict import predict_img, mask_to_image
+from predict import mask_to_image
+from utils.data_loading import BasicDataset
+from utils.utils import get_prediction_debug_image
 
 
-def main():
-    test_name = "test"
-    input_filepath = Path(f"data_floorplan/{test_name}/floorplan_resized.png")
-
-    img = Image.open(input_filepath)
-
-    if img.mode == "RGBA":
-        img = img.convert("RGB")
-
-    net = UNet(n_channels=3, n_classes=2)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    net.to(device=device)
-
-    predict_multiple_epochs = True
+def get_model_names():
+    predict_multiple_epochs = False
     if predict_multiple_epochs:
         start = 50
         end = 200
@@ -27,34 +18,88 @@ def main():
         epochs = range(start, end + 1, step)
         model_names = [f"checkpoint_epoch{epoch}.pth" for epoch in epochs]
     else:
-        model_names = ["checkpoint_epoch200.pth"]
+        model_names = ["checkpoint_epoch50.pth"]
 
-    run_dir = Path("runs/Feb12_17-56-30_tiso_augmentation_LR_1e-08_BS_1_SCALE_1.0")
-    run_dir = Path("runs/Mar25_13-46-20_tiso_LR_1e-08_BS_1_SCALE_1.0")
+    return model_names
+
+
+def get_output_dir(model_names, i, output_dir):
+    if len(model_names) == 1:
+        return output_dir
+
+    output_dir2 = output_dir / model_names[i].split(".")[0]
+    output_dir2.mkdir(exist_ok=True)
+    return output_dir2
+
+
+def main():
+    net = UNet(n_channels=3, n_classes=2)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net.to(device=device)
+
+    root_dir = Path("dataset_floorplan/2025_03_13/test")
+    input_dir = root_dir / "imgs"
+    input_files = list(input_dir.glob("*.png"))
+    input_files.sort()
+
+    run_dir = Path(
+        "runs2/2025-04-11_09-39-16_enable_augmentation_BS_1_LR_1e-05_SCALE_1.0"
+    )
     model_dir = run_dir / "checkpoints"
 
-    out_dir = run_dir / test_name
-    out_dir.mkdir(exist_ok=True)
+    output_dir = root_dir / "masks"
+    output_dir.mkdir(exist_ok=True)
 
-    img.save(out_dir / "input.png")
+    model_names = get_model_names()
 
-    for model_name in model_names:
+    for i in range(len(model_names)):
+        model_name = model_names[i]
+        print(f"Using model {model_name}...")
         model_filepath = model_dir / model_name
         state_dict = torch.load(model_filepath, map_location=device)
         mask_values = state_dict.pop("mask_values", [0, 1])
         net.load_state_dict(state_dict)
+        # net.eval()
 
-        mask = predict_img(
-            net=net,
-            full_img=img,
-            device=device,
-        )
+        for input_filepath in input_files:
+            print(f"Predicting {input_filepath.name}...")
+            img = (
+                BasicDataset.get_preprocessed_image(
+                    filepath=input_filepath, mask_values=mask_values
+                )
+                .to(device=device)
+                .unsqueeze(0)
+            )
 
-        result = mask_to_image(mask, mask_values)
+            with torch.autocast(
+                device.type if device.type != "mps" else "cpu", enabled=True
+            ):
+                prediction = net(img)
+                prediction = prediction.float().cpu()
+                pred_mask = prediction.argmax(dim=1)
 
-        model_name = model_name.split(".")[0]
-        output_filepath = out_dir / f"{model_name}_output.png"
-        result.save(output_filepath)
+                result = mask_to_image(
+                    pred_mask.squeeze(0).numpy(),
+                    mask_values,
+                )
+
+                model_name_output_dir = get_output_dir(model_names, i, output_dir)
+                output_filepath = model_name_output_dir / f"{input_filepath.stem}.gif"
+                result.save(output_filepath)
+
+                img = img.to(device="cpu")
+                pred_mask = pred_mask.to(device="cpu")
+                prediction_debug_image = get_prediction_debug_image(
+                    img.squeeze(), prediction, pred_mask
+                )
+
+                debug_image_filepath = (
+                    model_name_output_dir / f"{input_filepath.stem}_debug.png"
+                )
+
+                save_image(prediction_debug_image, debug_image_filepath)
+
+                torch.cuda.empty_cache()
 
 
 if __name__ == "__main__":
